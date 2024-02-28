6fe62e8855b2b16fa63cf28b02bf01b2a0be0e94
Vinayreddysama <Vinayreddysama1211@gmail.com>
2024-02-27 22:00:40 -0500

    Added Home page

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function validateLogin() {
       alert("Username and Password are required");
     } else {
       // Redirect to the dashboard screen (you can replace 'dashboard.html' with the actual path)
-      window.location.href = "index.html";
+      window.location.href = "Home.html";
     }
   }
   
