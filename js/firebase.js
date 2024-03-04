f31d384ceb2dc7c68700af7033c6f3bc28c005cf
Vinayreddysama <Vinayreddysama1211@gmail.com>
2024-03-03 20:40:12 -0500

    Added FirebaSE

---
+++ js/firebase.js
@@ -0,0 +1,56 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCr9WYSZ_u1aE_4tbuWearPTF_AYLyWogM",
+    authDomain: "mytickets-71aca.firebaseapp.com",
+    projectId: "mytickets-71aca",
+    storageBucket: "mytickets-71aca.appspot.com",
+    messagingSenderId: "404007165875",
+    appId: "1:404007165875:web:1309b449460fa2cf2475c6",
+    measurementId: "G-T21B1R8FNK"
+  };
+  
+  // initialize firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // reference your database
+  var contactFormDB = firebase.database().ref("contactForm");
+  
+  document.getElementById("contactForm").addEventListener("submit", submitForm);
+  
+  function submitForm(e) {
+    e.preventDefault();
+  
+    var name = getElementVal("name");
+    var emailid = getElementVal("emailid");
+    var msgContent = getElementVal("msgContent");
+  
+    saveMessages(name, emailid, msgContent);
+  
+    //   enable alert
+    document.querySelector(".alert").style.display = "block";
+  
+    //   remove the alert
+    setTimeout(() => {
+      document.querySelector(".alert").style.display = "none";
+    }, 3000);
+  
+    //   reset the form
+    document.getElementById("contactForm").reset();
+  }
+  
+  const saveMessages = (name, emailid, msgContent) => {
+    var newContactForm = contactFormDB.push();
+  
+    newContactForm.set({
+      name: name,
+      emailid: emailid,
+      msgContent: msgContent,
+    });
+  };
+  
+  const getElementVal = (id) => {
+    return document.getElementById(id).value;
+  };
